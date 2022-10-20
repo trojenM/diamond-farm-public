f648a8f89e024d38c48ef4de32913de80161456b
kabusogi <dogukan4833@gmail.com>
2022-10-20 19:56:18 +0300

    ADD hammer and destruction logic

--- Assets/Scripts/DorudonGames/Runtime/Component/HammerComponent.cs
+++ Assets/Scripts/DorudonGames/Runtime/Component/HammerComponent.cs
@@ -1,9 +1,13 @@
+using DorudonGames.Runtime.Manager;
 using UnityEngine;
 
 namespace DorudonGames.Runtime.Component
 {
     public class HammerComponent : MonoBehaviour
     {
+        [SerializeField] private ParticleSystem sparkleParticle;
+        [SerializeField] private LayerMask layerMask;
+        [SerializeField] private Transform castPosition;
         [SerializeField] private float sphereRadius;
         [SerializeField] private float rotateDegree;
         [SerializeField] private float rotationDistance = 0.025f;
@@ -11,12 +15,14 @@ namespace DorudonGames.Runtime.Component
         private Quaternion _startRotation, _endRotation; 
         private Quaternion _targetRotation;
         private bool _rotateDown = true;
+        private Collider _collider;
 
 
         private static float _rotateSpeed = 30f;
 
         private void Awake()
         {
+            _collider = GetComponent<Collider>();
             _startRotation = transform.rotation;
             _endRotation = Quaternion.Euler(_startRotation.eulerAngles + new Vector3(0f, 0f, rotateDegree));
             SetTargetDown();
@@ -31,14 +37,16 @@ namespace DorudonGames.Runtime.Component
 
         private void OnTriggerEnter(Collider other)
         {
-        
+            if (!other.GetComponent<PieceComponent>()) return;
+            
+            OnHammerHit();
         }
 
         private void RotateHammer()
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation,_rotateSpeed*Time.deltaTime);
         }
-
+        
         private void SwapTargetRotation()
         {
             if (_rotateDown)
@@ -55,12 +63,31 @@ namespace DorudonGames.Runtime.Component
         {
             _rotateDown = false;
             _targetRotation = _startRotation;
+            _collider.enabled = false;
         }
 
         private void SetTargetDown()
         {
             _rotateDown = true;
             _targetRotation = _endRotation;
+            _collider.enabled = true;
+        }
+
+        private void OnHammerHit()
+        {
+            sparkleParticle.Play();
+            SetTargetUp();
+
+            RaycastHit[] hitInfo = Physics.SphereCastAll(castPosition.position, sphereRadius, castPosition.forward, 10f, layerMask,QueryTriggerInteraction.UseGlobal);
+            
+            foreach (var hit in hitInfo)
+            {
+                if (hit.transform.TryGetComponent(out PieceComponent piece))
+                {
+                    piece.rb.isKinematic = false;
+                    piece.rb.mass = 10;
+                }
+            }
         }
 
         public static void SetRotateSpeed(float speed) => _rotateSpeed = speed;
--- Assets/Scripts/DorudonGames/Runtime/Component/PieceComponent.cs
+++ Assets/Scripts/DorudonGames/Runtime/Component/PieceComponent.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PieceComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
         
